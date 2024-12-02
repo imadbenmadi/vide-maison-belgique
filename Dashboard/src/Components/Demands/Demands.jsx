53957c74b63fad823b7b633c755c531e7a57efe6
imadbenmadi <imadbenmadi891@gmail.com>
2024-12-02 18:44:45 +0100

    feat: add new components for FAQ, Main, About, Services, and Demands with basic structure

---
+++ Dashboard/src/Components/Demands/Demands.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+import Swal from "sweetalert2";
+import axios from "axios";
+import Card from "./Card";
+import { BiMessage } from "react-icons/bi";
+
+function Demands() {
+    const Navigate = useNavigate();
+
+    const [Demands, setDemands] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    useEffect(() => {
+        setLoading(true);
+        const FetchDemands = async ({ setDemands, setLoading, setError }) => {
+            setLoading(true);
+            try {
+                const response = await axios.get(
+                    `http://localhost:3000/Admin/Demands`,
+                    {
+                        withCredentials: true,
+                        validateStatus: () => true,
+                    }
+                );
+                if (response.status == 200) {
+                    const Demands = response.data.Demands;
+                    setDemands(Demands);
+                } else if (response.status == 401) {
+                    Swal.fire(
+                        "Error",
+                        "يجب عليك تسجيل الدخول مرة اخرى",
+                        "error"
+                    );
+                    Navigate("/Login");
+                } else {
+                    setError(response.data);
+                }
+            } catch (error) {
+                setError(error);
+            } finally {
+                // setLoading(false);
+            }
+        };
+
+        FetchDemands({ setDemands, setLoading, setError }).then(() => {
+            setLoading(false);
+        });
+    }, []);
+
+    if (loading) {
+        return (
+            <div className=" w-[100vw] h-[80vh] flex flex-col items-center justify-center">
+                <span className="loader"></span>
+            </div>
+        );
+    } else if (error)
+        return (
+            <div className=" w-[100vw] h-screen flex items-center justify-center">
+                <div className="text-red-600 font-semibold">{error.demand}</div>
+            </div>
+        );
+    else
+        return (
+            <div className=" py-6 px-4">
+                <div className=" text-xl font-semibold text-yallow_v">
+                    {" "}
+                    Demands Demands
+                </div>
+
+                {!Demands || Demands?.length == 0 ? (
+                    <div className="text-md font-semibold text-gray_v text-center pt-12">
+                        No Demands yet
+                    </div>
+                ) : (
+                    <div>
+                        <div className=" w-full flex justify-center py-4">
+                            <div className="max-w-[300px] border shadow-md py-6 px-6 flex flex-col items-center justify-start rounded-md md:min-w-[200px]">
+                                <div className=" text-xs font-semibold pb-5 text-gray_v w-full">
+                                    Total Number of demands:
+                                </div>
+                                <div className=" flex justify-between gap-2 mx-2 w-full">
+                                    <div className="  font-semibold text-2xl">
+                                        {Demands?.length}
+                                    </div>
+                                    <div className=" shrink-0 text-yallow_v border border-gray_white px-2 py-1 flex items-center justify-center rounded-lg shadow-lg">
+                                        <BiMessage className=" shrink-0 text-2xl" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {Demands &&
+                            Demands?.map((demand) => {
+                                return (
+                                    <Card
+                                        key={demand.id}
+                                        Message={demand}
+                                        setDemands={setDemands}
+                                        Demands={Demands}
+                                        // handle_Delete_demand={handle_Delete_demand}
+                                    />
+                                );
+                            })}
+                    </div>
+                )}
+            </div>
+        );
+}
+
+export default Demands;
--- Dashboard/src/Components/NavBar/Mobile/Mobile_Nav_Items.jsx
+++ Dashboard/src/Components/NavBar/Mobile/Mobile_Nav_Items.jsx
@@ -1,22 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router";
 import { useNavigate } from "react-router";
-import { useEffect } from "react";
 import { useAppContext } from "../../../AppContext";
 import { TbLogout2 } from "react-icons/tb";
 import axios from "axios";
 import Swal from "sweetalert2";
 import { Link } from "react-router-dom";
 import { GoHome } from "react-icons/go";
-import { PiListFill } from "react-icons/pi";
 import { RiContactsLine } from "react-icons/ri";
-import { FaRegBuilding } from "react-icons/fa";
-import { MdOutlineAddCircleOutline } from "react-icons/md";
+import { FaRegEdit } from "react-icons/fa";
+import { MdOutlineWork } from "react-icons/md";
+
 function Mobile_Nav_Items({ MobileNav_Open, Toogle_Menu_Bar }) {
     const Navigate = useNavigate();
     const { set_Auth } = useAppContext();
     const [Active_nav, setActive_nav] = useState("Home");
     const location = useLocation();
+
     useEffect(() => {
         setActive_nav(location.pathname.split("/")[1]);
     }, [location.pathname]);
@@ -25,16 +25,12 @@ function Mobile_Nav_Items({ MobileNav_Open, Toogle_Menu_Bar }) {
     const handleLogout = async () => {
         setLogoutClicked(true);
         try {
-            // Send a request to the logout endpoint on the server
             const response = await axios.post(
                 "http://localhost:3000/logout",
                 {},
-                {
-                    withCredentials: true,
-                    validateStatus: () => true,
-                }
+                { withCredentials: true, validateStatus: () => true }
             );
-            if (response.status == 204) {
+            if (response.status === 204) {
                 set_Auth(false);
                 Swal.fire("Success!", `Logged Out Successfully`, "success");
                 Navigate("/Login");
@@ -46,137 +42,130 @@ function Mobile_Nav_Items({ MobileNav_Open, Toogle_Menu_Bar }) {
         }
         setLogoutClicked(false);
     };
+
     return (
         <div className="flex md:hidden">
             <div
-                className={`  ${
-                    MobileNav_Open
-                        ? " translate-x-[0vw]"
-                        : " translate-x-[200vh] "
-                } absolute   transition-transform duration-300 select-none w-[100vw]
-                  z-50    text-black_text  bg-white `}
+                className={`${
+                    MobileNav_Open ? "translate-x-[0vw]" : "translate-x-[200vh]"
+                } absolute transition-transform duration-300 select-none w-[100vw] z-50 text-black_text bg-white`}
             >
                 <div className="flex flex-col gap-8 text-sm text-black_text pl-8 py-4 h-screen overflow-auto">
+                    <div className="flex flex-col items-center justify-center">
+                        <img
+                            src={"../../../public/Logo.png"}
+                            alt="Logo"
+                            className="w-16"
+                        />
+                    </div>
                     <div>
-                        <div className=" font-semibold pb-4 flex items-center gap-2">
-                            <GoHome className=" text-lg" />
-                            Home
-                        </div>
-                        <div className=" flex flex-col gap-2 pl-2  ">
+                        <div className="flex flex-col gap-2 my-6">
                             <Link
-                                to={"/Home"}
+                                to="/Home"
                                 onClick={Toogle_Menu_Bar}
-                                className={` ${
-                                    Active_nav == "Home"
-                                        ? "bg-yallow_v text-white px-4 "
+                                className={`${
+                                    Active_nav === "Home"
+                                        ? "bg-yallow_v text-white px-4"
                                         : "bg-white hover:text-yallow_v"
-                                }  transition-all duration-150  cursor-pointer py-1 select-none  w-[150px] rounded-full  `}
+                                } transition-all duration-150 cursor-pointer py-1 w-[150px] rounded-full`}
                             >
-                                <span>Home</span>
+                                <GoHome className="text-lg" /> Home
                             </Link>
+                        </div>
+                        <div>
+                            <div className="font-semibold pb-4 flex gap-2 items-center">
+                                <FaRegEdit className="text-lg" /> Edit Content
+                            </div>
+                            <div className="flex flex-col gap-2 pl-2">
+                                <Link
+                                    to="/Edit_Main"
+                                    onClick={Toogle_Menu_Bar}
+                                    className={`${
+                                        Active_nav === "Edit_Main"
+                                            ? "bg-yallow_v text-white px-4"
+                                            : "bg-white hover:text-yallow_v"
+                                    } transition-all duration-150 cursor-pointer py-1 w-[150px] rounded-full`}
+                                >
+                                    Main Page
+                                </Link>
+                                <Link
+                                    to="/Edit_About"
+                                    onClick={Toogle_Menu_Bar}
+                                    className={`${
+                                        Active_nav === "Edit_About"
+                                            ? "bg-yallow_v text-white px-4"
+                                            : "bg-white hover:text-yallow_v"
+                                    } transition-all duration-150 cursor-pointer py-1 w-[150px] rounded-full`}
+                                >
+                                    About
+                                </Link>
+                                <Link
+                                    to="/Edit_FAQ"
+                                    onClick={Toogle_Menu_Bar}
+                                    className={`${
+                                        Active_nav === "Edit_FAQ"
+                                            ? "bg-yallow_v text-white px-4"
+                                            : "bg-white hover:text-yallow_v"
+                                    } transition-all duration-150 cursor-pointer py-1 w-[150px] rounded-full`}
+                                >
+                                    FAQ
+                                </Link>
+                                <Link
+                                    to="/Edit_Services"
+                                    onClick={Toogle_Menu_Bar}
+                                    className={`${
+                                        Active_nav === "Edit_Services"
+                                            ? "bg-yallow_v text-white px-4"
+                                            : "bg-white hover:text-yallow_v"
+                                    } transition-all duration-150 cursor-pointer py-1 w-[150px] rounded-full`}
+                                >
+                                    Services
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="font-semibold flex gap-2 items-center">
                             <Link
-                                to={"/Users"}
+                                to="/Demands"
                                 onClick={Toogle_Menu_Bar}
-                                className={` ${
-                                    Active_nav == "Users"
-                                        ? "bg-yallow_v text-white px-4 "
+                                className={`${
+                                    Active_nav === "Demands"
+                                        ? "bg-yallow_v text-white px-4"
                                         : "bg-white hover:text-yallow_v"
-                                }  transition-all duration-150  cursor-pointer py-1 select-none  w-[150px] rounded-full  `}
+                                } transition-all duration-150 cursor-pointer py-1 w-[150px] rounded-full`}
                             >
-                                <span>Users</span>
+                                <MdOutlineWork className="text-lg" /> Demands
                             </Link>
                         </div>
                     </div>
                     <div>
-                        <div className=" font-semibold pb-4 flex items-center gap-2">
-                            {/* <MdOutlineWorkOutline className=" text-lg" /> */}
-                            Medical Institustions
-                        </div>{" "}
-                        <div className=" flex flex-col gap-2 pl-2 ">
+                        <div className="font-semibold pb-4 flex gap-2 items-center">
                             <Link
-                                to={"/Institustions"}
+                                to="/Contact"
                                 onClick={Toogle_Menu_Bar}
-                                className={` ${
-                                    Active_nav == "Institustions"
-                                        ? "bg-yallow_v text-white px-4 "
+                                className={`${
+                                    Active_nav === "Contact"
+                                        ? "bg-yallow_v text-white px-4"
                                         : "bg-white hover:text-yallow_v"
-                                }  transition-all duration-150  cursor-pointer py-1 select-none 
-                                flex items-center gap-2 
-                                    w-[150px] rounded-full  `}
+                                } transition-all duration-150 cursor-pointer py-1 w-[150px] rounded-full`}
                             >
-                                <FaRegBuilding className=" text-lg" />
-                                <span>Institustions</span>
-                            </Link>
-                            <Link
-                                to={"/New_institution"}
-                                onClick={Toogle_Menu_Bar}
-                                className={` ${
-                                    Active_nav == "New_institution"
-                                        ? "bg-yallow_v text-white px-4 "
-                                        : "bg-white hover:text-yallow_v"
-                                }  transition-all duration-150  cursor-pointer py-1 select-none 
-                                flex items-center gap-2 
-                                    w-[180px] rounded-full  `}
-                            >
-                                <MdOutlineAddCircleOutline className=" text-lg" />
-
-                                <span> new institution</span>
-                            </Link>
-                        </div>
-                    </div>
-
-                    {/* <div>
-                        <div className=" font-semibold pb-4 flex items-center gap-2">
-                            <PiListFill className=" text-lg" />
-                            Terms
-                        </div>{" "}
-                        <div className=" flex flex-col gap-2 pl-2 ">
-                            <Link
-                                to={"/Terms"}
-                                onClick={Toogle_Menu_Bar}
-                                className={` ${
-                                    Active_nav == "Terms"
-                                        ? "bg-yallow_v text-white px-4 "
-                                        : "bg-white hover:text-yallow_v"
-                                }  transition-all duration-150  cursor-pointer py-1 select-none  w-[150px] rounded-full  `}
-                            >
-                                <span>Terms of service</span>
-                            </Link>
-                        </div>
-                    </div> */}
-                    <div>
-                        <div className=" font-semibold pb-4 flex items-center gap-2">
-                            <RiContactsLine className=" text-lg" />
-                            Contact
-                        </div>{" "}
-                        <div className=" flex flex-col gap-2 pl-2  ">
-                            <Link
-                                to={"/Contact"}
-                                onClick={Toogle_Menu_Bar}
-                                className={` ${
-                                    Active_nav == "Contact"
-                                        ? "bg-yallow_v text-white px-4 "
-                                        : "bg-white hover:text-yallow_v"
-                                }  transition-all duration-150  cursor-pointer py-1 select-none  w-[150px] rounded-full  `}
-                            >
-                                <span> Messages</span>
+                                <RiContactsLine className="text-lg" /> Contact
+                                Messages
                             </Link>
                         </div>
                     </div>
                     <div className="pb-20">
                         {LogoutClicked ? (
-                            <div className="w-full ">
-                                <span className="small-loader font-bold  w-full m-auto"></span>
+                            <div className="w-full">
+                                <span className="small-loader font-bold w-full m-auto"></span>
                             </div>
                         ) : (
                             <div
-                                className="cursor-pointer w-full 
-                                    flex items-center gap-3 text-red-500"
-                                onClick={() => {
-                                    handleLogout();
-                                }}
+                                className="cursor-pointer w-full flex items-center gap-3 text-red-500"
+                                onClick={handleLogout}
                             >
-                                <TbLogout2 className="  text-xl" />
+                                <TbLogout2 className="text-xl" />
                                 Logout
                             </div>
                         )}
