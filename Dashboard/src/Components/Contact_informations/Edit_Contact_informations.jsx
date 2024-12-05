dbda8882647490786c812c9fbf8593500d2dc997
imadbenmadi <imadbenmadi891@gmail.com>
2024-12-05 16:38:28 +0100

    style: center header text in Edit_Contact_informations component for improved aesthetics

--- Dashboard/src/Components/Contact_informations/Edit_Contact_informations.jsx
+++ Dashboard/src/Components/Contact_informations/Edit_Contact_informations.jsx
@@ -86,7 +86,7 @@ const Edit_Contact_informations = () => {
 
     return (
         <div className="max-w-3xl mx-auto p-6 bg-white shadow-lg rounded-lg">
-            <h2 className="text-2xl font-semibold mb-6">
+            <h2 className="text-2xl font-semibold mb-6 text-center text-black_text">
                 Edit Contact Information
             </h2>
             <Formik
