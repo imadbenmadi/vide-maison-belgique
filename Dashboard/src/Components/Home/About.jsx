31df3b1261a692e185913df907e97375882cb4b0
imadbenmadi <imadbenmadi891@gmail.com>
2025-01-08 14:50:16 +0100

    feat: enhance About component with image loading, fallback handling, and in-view animation

--- Dashboard/src/Components/Home/About.jsx
+++ Dashboard/src/Components/Home/About.jsx
@@ -1,12 +1,20 @@
-import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
+import React, { useState, useEffect, useRef } from "react";
+import { motion, useInView } from "framer-motion";
 
 function About({ data }) {
     const [imageSrc, setImageSrc] = useState(""); // Manage the current image source
     const fallbackImage = "/Image-not-found.png"; // Path to your fallback image (make sure it's correct)
+
+    const ref = useRef(null); // Reference to track the section
+    const isInView = useInView(ref, {
+        once: true,
+        margin: "0px 0px -100px 0px",
+    }); // Trigger only once when 100px above the viewport
+
     useEffect(() => {
         console.log(data);
     }, [data]);
+
     useEffect(() => {
         const loadImage = (src) => {
             const img = new Image();
@@ -38,20 +46,20 @@ function About({ data }) {
     };
 
     return (
-        <div className="relative min-h-[70vh] bg-cover bg-center mt-12 ">
-            <div className=" text-4xl font-semibold  text-gray-700 text-center mb-16 ">
+        <div className="relative min-h-[70vh] bg-cover bg-center my-12">
+            <div className="text-4xl font-semibold text-gray-700 text-center mb-8 md:mb-16">
                 À propos de nous
             </div>
-            <div className=" flex flex-col md:gap-16 md:flex-row items-center justify-center  ">
-                <div className="md:w-1/2 text-center md:text-start">
+            <div className="flex flex-col md:gap-16 gap-6 md:flex-row items-center justify-center">
+                <div className="md:w-1/2 text-center md:text-start" ref={ref}>
                     {/* Animated Text Section */}
                     <motion.div
                         initial={{ opacity: 0, y: 50 }}
-                        animate={{ opacity: 1, y: 0 }}
+                        animate={isInView ? { opacity: 1, y: 0 } : {}}
                         transition={{ duration: 0.8, ease: "easeOut" }}
-                        className="text-start relative z-10"
+                        className="text-start relative z-10 " 
                     >
-                        <div className="text-3xl mb-1 text-gray-700 font-bold">
+                        <div className="text-2xl mb-1 text-gray-700 font-bold">
                             {data?.Title}
                         </div>
                         <div className="text-gray-700 text-lg">
@@ -66,7 +74,7 @@ function About({ data }) {
                     </motion.div>
                 </div>
 
-                <img src={imageSrc} alt="" className=" rounded-lg w-[220px]" />
+                <img src={imageSrc} alt="" className="rounded-lg w-[220px]" />
             </div>
         </div>
     );
