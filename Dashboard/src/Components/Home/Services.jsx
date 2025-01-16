1c26cc9f42beead9f025ba5ea7a7a821d44efc2a
imadbenmadi <imadbenmadi891@gmail.com>
2025-01-16 13:59:55 +0100

    feat: enhance Services component with improved image loading and fallback handling

--- Dashboard/src/Components/Home/Services.jsx
+++ Dashboard/src/Components/Home/Services.jsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Services({ data }) {
-    if (!data) return null;
     // useEffect(() => {
     //     console.log(data);
     // }, [data]);
 
+    if (!data) {
+        return null;
+    }
     return (
         <div className="py-6 px-4 min-h-screen">
             <div className="text-4xl font-semibold text-gray-800 text-center my-8">
@@ -32,26 +34,52 @@ function Services({ data }) {
 }
 
 function Service_card({ service }) {
+    if (!service) {
+        return null;
+    }
+    const fallbackImage = "/Image-not-found.png"; // Path to your fallback image (make sure it's correct)
+    const [imageSrc, setImageSrc] = useState(""); // Manage the current image source
+    useEffect(() => {
+        const loadImage = (src) => {
+            const img = new Image();
+            img.src = src;
+            img.onload = () => setImageSrc(src); // If image loads successfully
+            img.onerror = () => setImageSrc(fallbackImage); // If an error occurs
+        };
+
+        if (service?.image_link) {
+            loadImage(`http://localhost:3000${service.image_link}`);
+        } else {
+            setImageSrc(fallbackImage);
+        }
+    }, [service]);
+
     return (
         <div className="border rounded-lg shadow-md bg-white hover:shadow-lg transition-shadow duration-300">
             {service?.image_link && (
                 <img
                     loading="lazy"
-                    src={`http://localhost:3000${service?.image_link}`}
+                    // src={`http://localhost:3000${service?.image_link}`}
                     alt={service?.Title}
+                    src={imageSrc}
                     className="w-full h-48 object-cover rounded-t-lg"
                 />
             )}
 
             <div className="p-4">
-                <h4 className="text-lg font-semibold text-blue-700 mb-2  break-words">
+                <h4 className="text-lg font-semibold text-yallow_v text-center mb-2  break-words">
                     {service?.Title}
                 </h4>
                 <p className="text-gray-700 mb-4 break-words">
                     {service?.Description}
                 </p>
             </div>
-            <Link to={`/Home/Demand?Title=${service?.Title}`}>Demand</Link>
+            <Link
+                to={`/Home/Demand?Type=${service?.Type}`}
+                className="bg-yallow_v text-md py-2 px-4 text-white font-semibold rounded-b-lg w-full block text-center"
+            >
+                demande{" "}
+            </Link>
         </div>
     );
 }
